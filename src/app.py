dcd33945e99889698890617e0de17efc8eea6644
sun <sp900409@gmail.com>
2016-10-22 22:57:25 -0700

    add new_blog and new_post

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import make_response
 from flask import render_template
 from flask import request
 from flask import session
 
 from src.common.database import Database
 from src.models.blog import Blog
+from src.models.post import Post
 from src.models.user import User
 
 app = Flask(__name__)
@@ -67,12 +69,42 @@ def user_blogs(user_id=None):
     return render_template("user_blogs.html", blogs=blogs, email=user.email)
 
 
+@app.route('/blogs/new',methods=['POST', 'GET'])
+def create_new_blog():
+    if request.method =='GET':
+        return render_template('new_blog.html')
+    else:
+        title = request.form['title']
+        description = request.form['description']
+        user = User.get_by_email(session['email'])
+
+        new_blog = Blog(user.email, title, description, user._id)
+        new_blog.save_to_mongo()
+
+        return make_response(user_blogs(user._id))
+
 @app.route('/posts/<string:blog_id>')
 def blog_posts(blog_id):
     blog = Blog.from_mongo(blog_id)
     posts = blog.get_posts()
 
-    return render_template("posts.html", blog = blog, posts = posts)
+    return render_template("posts.html",  posts = posts, blog_id = blog._id, blog_title = blog.title)
+
+
+@app.route('/posts/new/<string:blog_id>',methods=['POST', 'GET'])
+def create_new_post(blog_id):
+    if request.method =='GET':
+        return render_template('new_post.html', blog_id=blog_id, )
+    else:
+        title = request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'])
+
+        new_post = Post(blog_id, title, content, user.email)
+        new_post.save_to_mongo()
+
+        return make_response(blog_posts(blog_id))
+
 
 if __name__ == '__main__':
     app.run(port=4989, debug=True)
